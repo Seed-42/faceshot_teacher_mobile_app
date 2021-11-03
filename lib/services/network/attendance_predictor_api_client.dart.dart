49dea3d6e7eb5771add603dd20255f5ecbc4cfa3
praharshbhatt <prahu.bhatt@gmail.com>
2021-11-03 13:52:19 +0530

    feat: implemented pipeline for face recognition API

--- lib/services/network/attendance_predictor_api_client.dart.dart
+++ lib/services/network/attendance_predictor_api_client.dart.dart
@@ -1,54 +1,67 @@
 // import 'dart:convert';
+import 'dart:convert';
 
 import 'package:faceshot_teacher/models/attendance.dart';
-// import 'package:faceshot_teacher/utils/const.dart';
-// import 'package:http/http.dart' as http_client;
+import 'package:faceshot_teacher/utils/const.dart';
+import 'package:http/http.dart' as http_client;
 
 ///Class for handling all the API calls to Attendance Predictor APIS
 class AttendancePredictorApiClient {
   ///Get Attendance prediction results
   static Future<List<Attendance>> getAttendancePrediction(
-      String classImage) async {
+    String classImage,
+  ) async {
     //Prepare the url
-    //String url = '${Const.apiBaseUrl}/get_prediction';
+    String url = '${Const.apiBaseUrl}/get_prediction';
+
+    //Prepare the body
+    Map body = {
+      'image': classImage,
+      'total_students': [],
+    };
 
     //Send the request
-    // final http_client.Response response = await http_client.get(
-    //   Uri.parse(url),
-    //   headers: <String, String>{'Accept': 'application/json'},
-    // );
+    final http_client.Response response = await http_client.post(
+      Uri.parse(url),
+      headers: <String, String>{'Accept': 'application/json'},
+      body: jsonEncode(body),
+    );
 
     //Handle the result
-    // final responseData = response.body;
-    // if (response.statusCode == 200) {
-    List<Attendance> markedAttendances = [];
-    List returnedAttendance = [
-      {
-        "student_id": "N3gAFHY4Cxew1bgXWnVUa2MYPuR2",
-        "attendance_confidence": 0.96,
-        "student_detected_face_coordinates": {
-          'topleft': [50, 50],
-          'topright': [100, 50],
-          'bottomright': [100, 100],
-          'bottomleft': [50, 100]
-        }
-      },
-      {
-        "student_id": "FI2Q5DEgUhUWXd6A6w7wy8dc2Kx1",
-        "attendance_confidence": 0.8,
-        "student_detected_face_coordinates": {
-          'topleft': [70, 70],
-          'topright': [700, 70],
-          'bottomright': [700, 700],
-          'bottomleft': [70, 700]
-        }
-      },
-    ];
+    final responseData = response.body;
+    if (response.statusCode == 200) {
+      List<Attendance> markedAttendances = [];
+      List returnedAttendance = jsonDecode(responseData);
+      // List returnedAttendance = [
+      //   {
+      //     "student_id": "N3gAFHY4Cxew1bgXWnVUa2MYPuR2",
+      //     "attendance_confidence": 0.96,
+      //     "student_detected_face_coordinates": {
+      //       'topleft': [50, 50],
+      //       'topright': [100, 50],
+      //       'bottomright': [100, 100],
+      //       'bottomleft': [50, 100]
+      //     }
+      //   },
+      //   {
+      //     "student_id": "FI2Q5DEgUhUWXd6A6w7wy8dc2Kx1",
+      //     "attendance_confidence": 0.8,
+      //     "student_detected_face_coordinates": {
+      //       'topleft': [70, 70],
+      //       'topright': [700, 70],
+      //       'bottomright': [700, 700],
+      //       'bottomleft': [70, 700]
+      //     }
+      //   },
+      // ];
 
-    for (var attendanceData in returnedAttendance) {
-      markedAttendances.add(Attendance.fromJson(attendanceData));
+      for (var attendanceData in returnedAttendance) {
+        markedAttendances.add(Attendance.fromJson(attendanceData));
+      }
+
+      return markedAttendances;
+    } else {
+      return [];
     }
-
-    return markedAttendances;
   }
 }
--- lib/utils/const.dart
+++ lib/utils/const.dart
@@ -1,5 +1,5 @@
 class Const {
   static String get apiBaseUrl {
-    return '';
+    return 'https://faceshot-backend-u4u5uuqxla-uc.a.run.app';
   }
 }
