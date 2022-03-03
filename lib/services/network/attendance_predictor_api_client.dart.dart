a212b85e77d3dbc36761f9048d4b3f1bf0a822fe
Praharsh Bhatt <praharsh.bhatt@sparrowconnected.com>
2022-03-02 23:19:46 -0500

    feat: add training api endpoint

--- lib/services/network/attendance_predictor_api_client.dart.dart
+++ lib/services/network/attendance_predictor_api_client.dart.dart
@@ -17,7 +17,9 @@ class AttendancePredictorApiClient {
     //Prepare the url
     Map? apiEndpoints = await FirestoreService.getPredictionApiEndpoint();
     String url = Const.apiBaseUrl;
-    if (apiEndpoints !=null && apiEndpoints.isNotEmpty && apiEndpoints.containsKey('face_recognition_api_endpoint')) {
+    if (apiEndpoints != null &&
+        apiEndpoints.isNotEmpty &&
+        apiEndpoints.containsKey('face_recognition_api_endpoint')) {
       url = apiEndpoints['face_recognition_api_endpoint'];
     }
 
@@ -36,7 +38,6 @@ class AttendancePredictorApiClient {
       // body: jsonEncode(body),
       body: body,
     );
-
 
     if (response.statusCode == 200) {
       log('result:\n' + response.body);
@@ -65,6 +66,55 @@ class AttendancePredictorApiClient {
       //   },
       // ];
 
+      for (var attendanceData in returnedAttendance) {
+        if (attendanceData['student_id'] != 'N/A') {
+          markedAttendances.add(Attendance.fromJson(attendanceData));
+        } else {
+          attendanceData['student_id'] = 'new student';
+          markedAttendances.add(Attendance.fromJson(attendanceData));
+        }
+      }
+
+      return markedAttendances;
+    } else {
+      return [];
+    }
+  }
+
+  ///Train Attendance with a new student
+  static Future<List<Attendance>> trainNewStudent(
+    String id,
+    List<String> images,
+  ) async {
+    //Prepare the url
+    Map? apiEndpoints = await FirestoreService.getPredictionApiEndpoint();
+    String url = Const.apiBaseUrl;
+    if (apiEndpoints != null &&
+        apiEndpoints.isNotEmpty &&
+        apiEndpoints.containsKey('base_prediction_api_endpoint')) {
+      url = apiEndpoints['base_prediction_api_endpoint'];
+    }
+
+    //Prepare the body
+    Map body = {
+      'id': id,
+      'images': images,
+    };
+
+    // log('Selected image:\n' + imageUrl);
+
+    //Send the request
+    final http_client.Response response = await http_client.post(
+      Uri.parse(url),
+      headers: <String, String>{'Accept': 'application/json'},
+      // body: jsonEncode(body),
+      body: body,
+    );
+
+    if (response.statusCode == 200) {
+      log('result:\n' + response.body);
+      List<Attendance> markedAttendances = [];
+      List returnedAttendance = jsonDecode(response.body)['result'];
       for (var attendanceData in returnedAttendance) {
         if (attendanceData['student_id'] != 'N/A') {
           markedAttendances.add(Attendance.fromJson(attendanceData));
